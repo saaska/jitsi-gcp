9bf60042b4eafa6c5d064866cb0b97681eeaa973
Александр Павлов <7911562+av-pavlov@users.noreply.github.com>
2022-11-17 17:17:37 +0400

    Change paths to work inside /usr/share/jitsi

--- setup-jitsi-instance.sh
+++ setup-jitsi-instance.sh
@@ -33,13 +33,14 @@ echo SETUP: Updated Docker package info>> /var/log/setup-jitsi.log
 sudo apt-get -y -q install docker-ce docker-ce-cli containerd.io docker-compose>>/var/log/setup-jitsi.log 2>&1
 echo SETUP: Installed Docker packages>> /var/log/setup-jitsi.log
 
-# Get jitsi-docker
+# Get jitsi-docker from GitHub Releases
 LATEST_JITSI_DOCKER=$(curl -s https://api.github.com/repos/jitsi/docker-jitsi-meet/tags | grep "tarball_url" | grep -Eo 'https://[^\"]*'| head -1)
 echo SETUP: Downloading $LATEST_JITSI_DOCKER>> /var/log/setup-jitsi.log
 mkdir docker-jitsi-meet
 curl -sL $LATEST_JITSI_DOCKER | tar xz -C docker-jitsi-meet
 echo SETUP: Downloaded latest docker-jitsi-meet release>> /var/log/setup-jitsi.log
-mv docker-jitsi-meet/* /usr/share/jitsi
+mkdir /usr/share/jitsi
+mv docker-jitsi-meet/* /usr/share/jitsi/docker-jitsi-meet
 
 # Install Google Cloud Ops Agent for monitoring
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
@@ -47,7 +48,7 @@ echo SETUP: Installed Google Cloud Ops Agent>> /var/log/setup-jitsi.log
 
 # Get Let's encrypt SSL keys
 sudo apt-get -y -q install jq  # install JSON processor for the keys obtained from secrets
-sudo mkdir /opt/jitsi-docker/certs
+sudo mkdir /usr/share/jitsi/certs
 META_URL=http://metadata.google.internal/computeMetadata/v1
 META_HEADER="Metadata-Flavor: Google"
 TOKEN=$(gcloud auth print-access-token)
@@ -56,14 +57,14 @@ KEY_SECRET_NAME=$(curl -s "$META_URL/instance/attributes/keysecret" -H "$META_HE
 FULLCHAIN_SECRET_NAME=$(curl -s "$META_URL/instance/attributes/fullchainsecret" -H "$META_HEADER")
 curl -s https://secretmanager.googleapis.com/v1/projects/$PROJECT_ID/secrets/$KEY_SECRET_NAME/versions/latest:access  \
    --request "GET" --header "authorization: Bearer $TOKEN" --header "content-type: application/json" --silent \
-   | jq -r ".payload.data" | base64 --decode | sudo tee /opt/jitsi-docker/certs/key.pem > /dev/null
+   | jq -r ".payload.data" | base64 --decode | sudo tee /usr/share/jitsi/certs/key.pem > /dev/null
 curl -s https://secretmanager.googleapis.com/v1/projects/$PROJECT_ID/secrets/$FULLCHAIN_SECRET_NAME/versions/latest:access  \
    --request "GET" --header "authorization: Bearer $TOKEN" --header "content-type: application/json" --silent \
-   | jq -r ".payload.data" | base64 --decode | sudo tee /opt/jitsi-docker/certs/fullchain.pem > /dev/null
+   | jq -r ".payload.data" | base64 --decode | sudo tee /usr/share/jitsi/certs/fullchain.pem > /dev/null
 echo SETUP: Retrieved SSL certificates>> /var/log/setup-jitsi.log
 
 # Set up Jitsi config
-cd $(ls -d /opt/jitsi-docker/docker-jitsi*)
+cd /usr/share/jitsi/docker-jitsi-meet
 cp env.example .env
 ./gen-passwords.sh
 mkdir -p ~/.jitsi-meet-cfg/{web,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
@@ -72,7 +73,7 @@ HOSTNAME=$(curl -s "$META_URL/instance/attributes/hostname" -H "$META_HEADER")
 DOMAIN=$(curl -s "$META_URL/instance/attributes/domain" -H "$META_HEADER")
 sed -i -e 's/HTTP_PORT=8000/HTTP_PORT=80\nENABLE_HTTP_REDIRECT=1/' -e 's/HTTPS_PORT=8443/HTTPS_PORT=443/' -e "s%#PUBLIC_URL=https://meet.example.com%PUBLIC_URL=https://$HOSTNAME.$DOMAIN"% .env
 # Add cert path mapping
-sed -i -e "1,/prosody/ s%volumes:%volumes:\n            - /opt/jitsi-docker/certs/fullchain.pem:/config/keys/cert.crt\n            - /opt/jitsi-docker/certs/key.pem:/config/keys/cert.key%" docker-compose.yml
+sed -i -e "1,/prosody/ s%volumes:%volumes:\n            - /usr/share/jitsi/certs/fullchain.pem:/config/keys/cert.crt\n            - /usr/share/jitsi/certs/key.pem:/config/keys/cert.key%" docker-compose.yml
 echo SETUP: Configured Jitsi>> /var/log/setup-jitsi.log
 
 # start Jitsi
